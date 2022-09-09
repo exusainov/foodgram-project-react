5307de2d4cf3c3ed840f9dccfaa8d9f889f8f95f
Женя Хусаинов <evares92@gmail.com>
2022-09-09 23:03:19 +0500

    запуск докер работает 1.1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -112,20 +112,6 @@ class IngredientSerializer(serializers.ModelSerializer):
         read_only_fields = ('measurement_unit', 'name')
 
 
-class RecipeIngredientSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(
-        source='ingredient.id')
-    name = serializers.ReadOnlyField(
-        source='ingredient.name')
-    measurement_unit = serializers.ReadOnlyField(
-        source='ingredient.measurement_unit')
-
-    class Meta:
-        model = IngredientRecipe
-        fields = (
-            'id', 'name', 'measurement_unit', 'amount')
-
-
 class IngredientsEditSerializer(serializers.ModelSerializer):
 
     id = serializers.IntegerField()
