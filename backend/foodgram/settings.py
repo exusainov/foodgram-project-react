82c366d5f11c2167d2a1e6a42e95510ee058f20a
Женя Хусаинов <evares92@gmail.com>
2022-09-12 22:58:53 +0500

    Статика настроена деплой работает

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -2,14 +2,14 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECRET_KEY = os.getenv('SECRET_KEY') #
-SECRET_KEY = 'grue9w^_8oq*t#skc0o!q3evkxisg0zor5v_smw^@6wf!r-irv'
+SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = 'grue9w^_8oq*t#skc0o!q3evkxisg0zor5v_smw^@6wf!r-irv'
 # SECRET_KEY = (
 # 'SECRET_KEY',
 # 'my_mega_secret_code_ilz@4zqj=rq&agdol^##zgl9(vs')
