71f97e3f9b0bdd6f537f604a85e577d9e8cd71a5
Evgeniy Husainov <evares92@gmail.com>
2022-09-13 17:51:13 +0500

    частичная корректировка кода 1.0

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -82,17 +82,6 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'slug', 'color')
 
 
-# class Base64ImageField(serializers.ImageField):
-#     def to_internal_value(self, data):
-#         if isinstance(data, str) and data.startswith('data:image'):
-#             format, imgstr = data.split(';base64,')
-#             ext = format.split('/')[-1]
-
-#             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-#         return super().to_internal_value(data)
-
-
 class IngredientRecipeSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(
         source='ingredient.id')
